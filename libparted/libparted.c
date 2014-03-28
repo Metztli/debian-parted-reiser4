8d5016bf8866a94438f4822d67e32a78422b4287
Colin Watson <cjwatson@ubuntu.com>
2014-03-28 17:09:48 +0000

    Add ZFS support
    Last-Update: 2014-07-30
    
    Patch-Name: zfs.patch

--- libparted/libparted.c
+++ libparted/libparted.c
@@ -110,6 +110,7 @@ extern void ped_file_system_fat_init (void);
 extern void ped_file_system_ext2_init (void);
 extern void ped_file_system_nilfs2_init (void);
 extern void ped_file_system_btrfs_init (void);
+extern void ped_file_system_zfs_init (void);
 
 static void
 init_file_system_types ()
@@ -126,6 +127,7 @@ init_file_system_types ()
 	ped_file_system_ext2_init ();
 	ped_file_system_nilfs2_init ();
 	ped_file_system_btrfs_init ();
+	ped_file_system_zfs_init ();
 }
 
 extern void ped_disk_aix_done ();
@@ -189,10 +191,12 @@ extern void ped_file_system_ufs_done (void);
 extern void ped_file_system_xfs_done (void);
 extern void ped_file_system_amiga_done (void);
 extern void ped_file_system_btrfs_done (void);
+extern void ped_file_system_zfs_done (void);
 
 static void
 done_file_system_types ()
 {
+	ped_file_system_zfs_done ();
 	ped_file_system_nilfs2_done ();
 	ped_file_system_ext2_done ();
 	ped_file_system_fat_done ();
