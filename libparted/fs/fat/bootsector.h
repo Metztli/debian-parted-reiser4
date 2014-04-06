f51f9b78e64885af878678d6ce159bc98f49900a
Phillip Susi <psusi@ubuntu.com>
2014-04-06 19:13:07 -0400

    Fix fat and ntfs detection on non 512 byte sectors
    The fixes for loop labels make use of fat and ntfs filesystem detection
    code to differentiate between those filesystem boot sectors and an msdos
    MBR, but the code crashed with non 512 byte sector sizes.
    
    Last-Update: 2014-07-07
    
    Patch-Name: fat-ntfs-large-sectors.patch

--- libparted/fs/fat/bootsector.h
+++ libparted/fs/fat/bootsector.h
@@ -116,11 +116,11 @@ struct __attribute__ ((packed)) _FatInfoSector {
         uint16_t	signature_3;	/* should be 0xaa55 */
 };
 
-int fat_boot_sector_read (FatBootSector* bs, const PedGeometry* geom);
+int fat_boot_sector_read (FatBootSector** bsp, const PedGeometry* geom);
 FatType fat_boot_sector_probe_type (const FatBootSector* bs,
 				    const PedGeometry* geom);
 int fat_boot_sector_analyse (FatBootSector* bs, PedFileSystem* fs);
 
-int fat_info_sector_read (FatInfoSector* is, const PedFileSystem* fs);
+int fat_info_sector_read (FatInfoSector** isp, const PedFileSystem* fs);
 
 #endif /* PED_FAT_BOOTSECTOR_H */
--- libparted/fs/fat/fat.c
+++ libparted/fs/fat/fat.c
@@ -34,7 +34,9 @@ fat_alloc (const PedGeometry* geom)
 	fs->type_specific = (FatSpecific*) ped_malloc (sizeof (FatSpecific));
 	if (!fs->type_specific)
 		goto error_free_fs;
-
+	FatSpecific* fs_info = (FatSpecific*) fs->type_specific;
+	fs_info->boot_sector = NULL;
+	fs_info->info_sector = NULL;
 	fs->geom = ped_geometry_duplicate (geom);
 	if (!fs->geom)
 		goto error_free_type_specific;
@@ -53,6 +55,8 @@ error:
 void
 fat_free (PedFileSystem* fs)
 {
+	FatSpecific* fs_info = (FatSpecific*) fs->type_specific;
+	free (fs_info->boot_sector);
 	ped_geometry_destroy (fs->geom);
 	free (fs->type_specific);
 	free (fs);
@@ -72,7 +76,7 @@ fat_probe (PedGeometry* geom, FatType* fat_type)
 
 	if (!fat_boot_sector_read (&fs_info->boot_sector, geom))
 		goto error_free_fs;
-	if (!fat_boot_sector_analyse (&fs_info->boot_sector, fs))
+	if (!fat_boot_sector_analyse (fs_info->boot_sector, fs))
 		goto error_free_fs;
 
 	*fat_type = fs_info->fat_type;
